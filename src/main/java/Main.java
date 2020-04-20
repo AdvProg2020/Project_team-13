56f609e3b4469de7735c03673d76d2b721cb8212
Peyman-hme <peyman.eb13@gmail.com>
2020-04-19 22:13:34 -0700

    Second Commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("I'm Hope So this project will work compeletly");
+        System.out.println("I'm Hope So this project will work compeletly "+ Test.sayHello());
     }
 }
---
+++ src/main/java/Test.java
@@ -0,0 +1,3 @@
+public class Test {
+    public static String sayHello(){return "hello";}
+}
