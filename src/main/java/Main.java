0aa9aa7f4333dc92cfa19674997a8661f53a0b4b
Peyman-hme <peyman.eb13@gmail.com>
2020-04-19 22:29:03 -0700

    Third Commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("I'm Hope So this project will work compeletly "+ Test.sayHello());
         System.out.println("Salam Chetori ");
+        System.out.println("yani vaghean dorost shod?");
     }
 }
