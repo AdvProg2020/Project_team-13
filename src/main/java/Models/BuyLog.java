3d2ad3fa77979d7956082ee459187b5610909e3b
sinamazaheri1379 <sinamazaheri1707@gmail.com>
2020-04-24 22:38:28 +0430

    MyThirdCommit

--- src/main/java/Models/BuyLog.java
+++ src/main/java/Models/BuyLog.java
@@ -1,6 +1,15 @@
 package Models;
 
+import Models.Product.Product;
+
+import java.util.ArrayList;
+import java.util.Date;
+
 public class BuyLog extends Log{
     private ReceivingStatus receivingStatus;
     private double discountAmount;
+
+    public BuyLog(String id, Date date, int price, String receiverUserName, String otherSideUserName, ArrayList<Product> allProducts) {
+        super(id, date, price, receiverUserName, otherSideUserName, allProducts);
+    }
 }
