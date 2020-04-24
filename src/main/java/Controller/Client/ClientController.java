31f16174625c879d461ba01a2802770c97013214
mohammadhnz <hmohammadali2013@gmail.com>
2020-04-24 23:48:54 +0430

    ClientController created.

--- src/main/java/Controller/Client/ClientController.java
+++ src/main/java/Controller/Client/ClientController.java
@@ -1,4 +1,22 @@
 package Controller.Client;
 
+import View.Menu;
+
 public class ClientController {
+    private static ClientController clientController;
+    private Menu currentMenu;
+
+    private ClientController() {
+    }
+
+    public static ClientController getInstance() {
+        if (clientController == null) {
+            clientController = new ClientController();
+        }
+        return clientController;
+    }
+
+    public void setCurrentMenu(Menu menu) {
+        this.currentMenu=menu;
+    }
 }
--- src/main/java/Models/Product/Category.java
+++ src/main/java/Models/Product/Category.java
@@ -13,23 +13,27 @@ public class Category {
     }
 
 
-    public void addProduct(Product product){
+    public void addProduct(Product product) {
         allProducts.add(product);
     }
 
     public ArrayList<String> getFeatureName() {
         return featureName;
     }
-    public void addFeature(String featureName){
+
+    public void addFeature(String featureName) {
         this.featureName.add(featureName);
     }
-    public void removeFeature(String featureName){
+
+    public void removeFeature(String featureName) {
         this.featureName.remove(featureName);
     }
-    public boolean isThisFeatureExists(String featureName){
+
+    public boolean isThisFeatureExists(String featureName) {
         return this.featureName.contains(featureName);
     }
-    public  void editFeatureName(String featureName){
+
+    public void editFeatureName(String featureName) {
 
     }
 
