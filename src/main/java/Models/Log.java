96a103adb6fa841fddd25bde9a52d9530c511119
sinamazaheri1379 <sinamazaheri1707@gmail.com>
2020-04-24 22:37:01 +0430

    MySecondCommit

--- src/main/java/Models/Log.java
+++ src/main/java/Models/Log.java
@@ -12,4 +12,61 @@ public class Log {
     private String receiverUserName;
     private String otherSideUserName;
     private ArrayList<Product> allProducts;
+
+    public Log(String id, Date date, int price, String receiverUserName, String otherSideUserName, ArrayList<Product> allProducts) {
+        this.id = id;
+        this.date = date;
+        this.price = price;
+        this.receiverUserName = receiverUserName;
+        this.otherSideUserName = otherSideUserName;
+        this.allProducts = allProducts;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getReceiverUserName() {
+        return receiverUserName;
+    }
+
+    public void setReceiverUserName(String receiverUserName) {
+        this.receiverUserName = receiverUserName;
+    }
+
+    public String getOtherSideUserName() {
+        return otherSideUserName;
+    }
+
+    public void setOtherSideUserName(String otherSideUserName) {
+        this.otherSideUserName = otherSideUserName;
+    }
+
+    public ArrayList<Product> getAllProducts() {
+        return allProducts;
+    }
+
+    public void setAllProducts(ArrayList<Product> allProducts) {
+        this.allProducts = allProducts;
+    }
 }
