dad594466980194148be238d70e0336bd0eafcb9
Peyman-hme <peyman.eb13@gmail.com>
2020-04-24 23:15:39 -0700

    Cart fixed

--- src/main/java/Models/Product/Cart.java
+++ src/main/java/Models/Product/Cart.java
@@ -3,30 +3,33 @@ package Models.Product;
 import Models.DiscountCode;
 import Models.UserAccount.Customer;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Cart {
     private Customer customer;
-    private HashMap<Product,Integer> countOfEachProduct;
+    private HashMap<String,Integer> countOfEachProduct;
     private DiscountCode discountCode;
+    private ArrayList<Product> allproduct;
     private double totalPrice=0;
     private String receivingInformation;
 
     public void addProduct(Product product){
         countOfEachProduct.put(product,1);
     }
-    public void changeCountOfProduct(Product product,int count){
-        countOfEachProduct.replace(product,countOfEachProduct.get(product)+count);
+    public void changeCountOfProduct(String productID,int count){
+        countOfEachProduct.replace(productID,countOfEachProduct.get(productID)+count);
     }
     public double getTotalPrice(){
         totalPrice=0;
-        for (Product product : countOfEachProduct.keySet()) {
-            totalPrice+=countOfEachProduct.get(product)*product.getProductCost();
+        for (String productID : countOfEachProduct.keySet()) {
+            Product product=getProductByID(productID);
+            totalPrice+=countOfEachProduct.get(productID)*product.getProductCost();
         }
         return totalPrice;
     }
     public Product getProductByID(String productID) {
-        for (Product product : countOfEachProduct.keySet()) {
+        for (Product product : allproduct) {
             if(product.getProductId().equals(productID)){
                 return product;
             }
