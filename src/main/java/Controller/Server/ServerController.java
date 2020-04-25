e31866c7e5d7d9ba1a7fe02de396c5109ce05a97
Peyman-hme <peyman.eb13@gmail.com>
2020-04-24 23:49:31 -0700

    getInstance() of ServerController

--- src/main/java/Controller/Server/ServerController.java
+++ src/main/java/Controller/Server/ServerController.java
@@ -1,4 +1,15 @@
 package Controller.Server;
 
 public class ServerController {
+    public static ServerController serverController;
+    private ServerController() {
+
+    }
+
+    public static ServerController getIncstance() {
+        if serverController == null){
+            serverController = new ServerController();
+        }
+        return serverController;
+    }
 }
