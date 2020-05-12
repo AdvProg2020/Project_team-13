13f97ae23b3be5197903158c2bf1b88db74851a5
mohammadhnz <hmohammadali2013@gmail.com>
2020-05-12 23:40:49 +0430

    sellermenu eddited;

--- src/main/java/View/UserMenu/Seller/SellerMenu.java
+++ src/main/java/View/UserMenu/Seller/SellerMenu.java
@@ -20,44 +20,26 @@ public class SellerMenu extends Menu {
     public void execute() {
         String command;
         System.out.println(ClientController.getInstance().getCurrentUser().viewPersonalInfo());
-        if (!ClientController.getInstance().getSeller().isAccepted()) {
-            System.out.println("Your registration application has not been approved by the administrator.");
-            while (!(command = scanner.nextLine()).equalsIgnoreCase("back")) {
-                if (command.equals("view personal info")) {
-                    System.out.println("You don't have access!");
-                } else if (command.equals("add product")) {
-                    System.out.println("You don't have access!");
-                } else if (command.equals("help")) {
-                    System.out.println("you can just get back!!");
-                } else if (command.equalsIgnoreCase("logout")) {
-                    ClientController.getInstance().setCurrentUser(null);
-                    System.out.println("You Logged out!!");
-                    parentMenu.execute();
-                } else {
-                    System.out.println("Invalid command");
-                }
+        boolean isAccepted = ClientController.getInstance().getSeller().isAccepted();
+        while (!(command = scanner.nextLine()).equalsIgnoreCase("back")) {
+            if (command.equals("view personal info") && isAccepted) {
+                System.out.println(ClientController.getInstance().getCurrentUser().viewPersonalInfo());
+            } else if (command.equals("manage products")&& isAccepted) {
+                Menu menu = new ManageProductMenu(this).setScanner(scanner);
+                ClientController.getInstance().setCurrentMenu(menu);
+                menu.execute();
+            } else if (command.equals("help")) {
+                help();
+            } else if (command.equalsIgnoreCase("logout")) {
+                ClientController.getInstance().setCurrentUser(null);
+                System.out.println("You Logged out!!");
+                parentMenu.execute();
+            } else {
+                System.out.println("Invalid command");
             }
-            back();
-        } else {
-            while (!(command = scanner.nextLine()).equalsIgnoreCase("back")) {
-                if (command.equals("view personal info")) {
-                    System.out.println(ClientController.getInstance().getCurrentUser().viewPersonalInfo());
-                } else if (command.equals("manage products")) {
-                    Menu menu = new ManageProductMenu(this).setScanner(scanner);
-                    ClientController.getInstance().setCurrentMenu(menu);
-                    menu.execute();
-                } else if (command.equals("help")) {
-                    help();
-                } else if (command.equalsIgnoreCase("logout")) {
-                    ClientController.getInstance().setCurrentUser(null);
-                    System.out.println("You Logged out!!");
-                    parentMenu.execute();
-                } else {
-                    System.out.println("Invalid command");
-                }
-            }
-            back();
         }
+        back();
+
 
     }
 
