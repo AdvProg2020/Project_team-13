189a6ae90d23e9def7625119886a563306befb41
Peyman-hme <peyman.eb13@gmail.com>
2020-05-19 10:38:19 -0700

    manager view personal info fixed

--- src/main/java/Models/UserAccount/Manager.java
+++ src/main/java/Models/UserAccount/Manager.java
@@ -4,17 +4,17 @@ public class Manager extends UserAccount {
 
     public Manager(String username, String password, String firstName, String lastName, String email, String phoneNumber, double credit) {
         super(username, password, firstName, lastName, email, phoneNumber, credit);
-        this.type="@Manager";
+        this.type = "@Manager";
     }
 
     @Override
     public String viewPersonalInfo() {
         String personalInfo = "";
-        personalInfo += this.username + "\n";
-        personalInfo += this.firstName + "\n";
-        personalInfo += this.lastName + "\n";
-        personalInfo += this.email + "\n";
-        personalInfo += this.phoneNumber ;
+        personalInfo += "User name: " + this.username + "\n";
+        personalInfo += "First Name: " + this.firstName + "\n";
+        personalInfo += "Last name: " + this.lastName + "\n";
+        personalInfo += "Email: " + this.email + "\n";
+        personalInfo += "PhoneNumber: " + this.phoneNumber;
         return personalInfo;
     }
 }
