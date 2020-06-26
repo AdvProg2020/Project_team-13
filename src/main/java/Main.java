0e1d1e80ff0930acaeb32c817ed57435267eb6a7
mohammadhnz <hmohammadali2013@gmail.com>
2020-06-26 16:19:35 +0430

    some changes.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import Controller.Server.ServerController;
+import Controller.Server.UserCenter;
 import Models.Product.Cart;
 import View.*;
 import javafx.application.Application;
@@ -6,13 +7,18 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
     public static void main(String[] args) {
-      launch();
+        launch();
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         new ServerController().runServer();
-        primaryStage.setTitle("Hello World");
-        new MainMenu(primaryStage).execute();
+        primaryStage.setTitle("Pms.com");
+        if (UserCenter.getIncstance().getAllManager() != null
+                && !UserCenter.getIncstance().getAllManager().isEmpty()) {
+            new MainMenu(primaryStage).execute();
+        }else {
+            new ManagerRegisterMenu(primaryStage).execute();
+        }
     }
 }
